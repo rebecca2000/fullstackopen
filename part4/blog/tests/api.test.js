7bb4894ce17fdb4ff06a6d4bff49d8b683c15404
rebecca2000 <rebecca.w8886@gmail.com>
2022-01-03 11:14:49 +1100

    Add test for POST request

--- part4/blog/tests/api.test.js
+++ part4/blog/tests/api.test.js
@@ -29,6 +29,19 @@ test('blog unique identifier is "ID"', async() => {
     }
 })
 
+test('POST request creates new blog post', async() => {
+    await new Blog({
+        'title': 'My blog',
+        'author': 'Shakespeare',
+        'url': 'www.blogs.com',
+        'likes': 10
+    }).save()
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(testTools.manyBlogs.length+1)
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('My blog')
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
